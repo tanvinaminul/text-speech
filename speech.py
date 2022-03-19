2a721795d65f1858451ef7ed5860fc1f2a561687
tanvinaminul <tanvinaminul@gmail.com>
2022-03-20 01:01:52 +0600

    text-audio

--- speech.py
+++ speech.py
@@ -1,3 +1,16 @@
 import azure.cognitiveservices.speech as speechsdk
 
 speech_config = speechsdk.SpeechConfig(subscription="7839171be3c84c828751b2d41e5b246d", region="eastus")
+# Note: if only language is set, the default voice of that language is chosen.
+speech_config.speech_synthesis_language = "en-US" # For example, "de-DE"
+# The voice setting will overwrite the language setting.
+# The voice setting will not overwrite the voice element in input SSML.
+speech_config.speech_synthesis_voice_name ="en-US-AriaNeural"
+
+audio_config = speechsdk.audio.AudioOutputConfig(filename="a.wav")
+synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+synthesizer.speak_text_async("A simple test to write to a file.")
+audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=None)
+result = synthesizer.speak_text_async("Getting the response as an in-memory stream.").get()
+stream = AudioDataStream(result)
