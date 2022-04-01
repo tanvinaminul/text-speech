86c1312e443e243f81d971babd356c6add288ee7
Aminul Islam Tanvin <tanvinaminul@gmail.com>
2022-04-02 00:14:39 +0600

    Update speech.py

--- speech.py
+++ speech.py
@@ -7,7 +7,7 @@ speech_config.speech_synthesis_language = "en-US" # For example, "de-DE"
 # The voice setting will not overwrite the voice element in input SSML.
 speech_config.speech_synthesis_voice_name ="YOUR_PREFERRED_VOICE"
 text=input("Input a text to make audio:\n")
-audio_config = speechsdk.audio.AudioOutputConfig(filename="YOUR_DESIRED_FILE_NAME")
+audio_config = speechsdk.audio.AudioOutputConfig(filename="YOUR_DESIRED_FILE_NAME") #out.wav
 synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 synthesizer.speak_text_async(text)
 audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
