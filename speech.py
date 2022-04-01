2c468bb1992b84f87002217a458ea5c4f7fbbc2c
tanvinaminul <tanvinaminul@gmail.com>
2022-04-01 23:47:22 +0600

    Generalized

--- speech.py
+++ speech.py
@@ -1,13 +1,13 @@
 import azure.cognitiveservices.speech as speechsdk
 
-speech_config = speechsdk.SpeechConfig(subscription="a307da45112a4384a70b8e0f68b47da5", region="eastus")
+speech_config = speechsdk.SpeechConfig(subscription="YOUR_SPEECH_SERVICE_KEY", region="REGION_OF_SERVICE")
 # Note: if only language is set, the default voice of that language is chosen.
 speech_config.speech_synthesis_language = "en-US" # For example, "de-DE"
 # The voice setting will overwrite the language setting.
 # The voice setting will not overwrite the voice element in input SSML.
-speech_config.speech_synthesis_voice_name ="en-US-AriaNeural"
+speech_config.speech_synthesis_voice_name ="YOUR_PREFERRED_VOICE"
 text=input("Input a text to make audio:\n")
-audio_config = speechsdk.audio.AudioOutputConfig(filename="a.wav")
+audio_config = speechsdk.audio.AudioOutputConfig(filename="YOUR_DESIRED_FILE_NAME")
 synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 synthesizer.speak_text_async(text)
 audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
